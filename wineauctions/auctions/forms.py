d3632e93bab00b6b99e351c1d7cca9c614184474
ivanse <elivanse@hotmail.com>
2023-09-18 18:41:32 -0300

    f

---
+++ wineauctions/auctions/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+
+from .models import *
+
+
+class listing_form(ModelForm):
+    class Meta:
+        model = listing
+        fields = ['item', 'price', 'description', 'category',  'photo']
+
+
+class bid_form(ModelForm):
+    class Meta:
+        model = bid
+        fields = ["price"]
+
+
+class comment_form(ModelForm):
+    class Meta:
+        model = comment
+        fields = ["title", "comment"]
---
+++ wineauctions/auctions/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("categories", views.categories_view, name="categories"),
+    path("categorie/<str:category>", views.add_category_view, name="show"),
+    path("add_watchlist/<int:id>",views.add_watchlist_view,name="add_watchlist"),
+    path("watchlist", views.watchlist_view, name="watchlist"),
+    path("comment/<int:id>", views.comment_view, name="comment"),
+    path("create_listings", views.create_listings_view, name="create_listings"),
+    path("listing/<int:id>", views.listings_view, name="listing"),
+    path("register", views.register_view, name="register"),
+]
+urlpatterns += staticfiles_urlpatterns()
