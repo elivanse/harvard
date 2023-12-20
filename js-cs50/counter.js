2106c67bb2fdff4ccc2f880c4365708f9dda0c59
Ivan Maximiliano Pereyra <elivanse@hotmail.com>
2023-12-19 22:58:11 -0300

    a

---
+++ js-cs50/counter.js
@@ -0,0 +1,24 @@
+
+if (!localStorage.getItem('counter')){
+  localStorage.setItem('counter',0);
+}
+    let counter = 0;
+
+    function count(){
+
+      counter++;
+      document.querySelector('h1').innerHTML = counter;
+
+      if (counter % 10===0){
+      
+        alert(`Count is now ${counter}`);
+
+      }
+
+    }
+    
+    document.addEventListener('DOMContentLoaded',function(){
+
+    document.querySelector('button').onclick =count;
+    
+    });
